453496cec86d828668515a2661c3cbc0c27b2e61
Leocano <Leocano@users.noreply.github.com>
2017-06-26 16:43:26 -0300

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default (props) => {
+  return (
+    <div className="container">
+      <div className="columns">
+        <div className="column has-text-centered">
+          <h1>Hello World!</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
