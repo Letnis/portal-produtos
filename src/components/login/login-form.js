565d977c3c3ab4035c8698f7dd9d4d18d46fc7a6
Leocano <Leocano@users.noreply.github.com>
2017-06-27 10:01:03 -0300

    "react-router-dom" adicionado

--- src/components/login/login-form.js
+++ src/components/login/login-form.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import LoginInput from './login-input';
+import MainButton from '../main-button';
 
 export default (props) => {
   return (
@@ -9,9 +10,10 @@ export default (props) => {
         <LoginInput label="Login" id="txt-login" type="text" placeholder="Digite o login" />
         <LoginInput label="Senha" id="txt-password" type="text" placeholder="Digite a senha" />
         <div className="field">
-          <button className="button">
-            Entrar
-          </button>
+          <MainButton text="Entrar" />
+        </div>
+        <div className="field">
+          <a href="" >Clique aqui para se cadastrar</a>
         </div>
       </div>
     </div>
---
+++ src/components/main-button.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default (props) => {
+  return (
+    <button className="button">
+      {props.text}
+    </button>
+  )
+}
